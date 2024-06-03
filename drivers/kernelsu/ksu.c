62427353cbbe038e55435d9817d779e116068d34
aminuddinihsan20 <aminuddin.ihsan47@gmail.com>
2024-06-03 09:53:03 +0700

    Revert "kernelsu: sync with repo"
    This reverts commit ec884981c450da8ae24d8d668dfbce3dad9f3e27.

--- drivers/kernelsu/ksu.c
+++ drivers/kernelsu/ksu.c
@@ -1,15 +1,13 @@
-#include <linux/export.h>
-#include <linux/fs.h>
-#include <linux/kobject.h>
-#include <linux/module.h>
-#include <linux/workqueue.h>
+#include "linux/fs.h"
+#include "linux/module.h"
+#include "linux/workqueue.h"
 
 #include "allowlist.h"
 #include "arch.h"
 #include "core_hook.h"
 #include "klog.h" // IWYU pragma: keep
 #include "ksu.h"
-#include "throne_tracker.h"
+#include "uid_observer.h"
 
 static struct workqueue_struct *ksu_workqueue;
 
@@ -32,10 +30,8 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 					    flags);
 }
 
-extern void ksu_sucompat_init();
-extern void ksu_sucompat_exit();
-extern void ksu_ksud_init();
-extern void ksu_ksud_exit();
+extern void ksu_enable_sucompat();
+extern void ksu_enable_ksud();
 
 int __init kernelsu_init(void)
 {
@@ -55,20 +51,13 @@ int __init kernelsu_init(void)
 
 	ksu_allowlist_init();
 
-	ksu_throne_tracker_init();
+	ksu_uid_observer_init();
 
 #ifdef CONFIG_KPROBES
-	ksu_sucompat_init();
-	ksu_ksud_init();
-#else
-	pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
+	ksu_enable_sucompat();
+	ksu_enable_ksud();
 #endif
 
-#ifdef MODULE
-#ifndef CONFIG_KSU_DEBUG
-	kobject_del(&THIS_MODULE->mkobj.kobj);
-#endif
-#endif
 	return 0;
 }
 
@@ -76,14 +65,9 @@ void kernelsu_exit(void)
 {
 	ksu_allowlist_exit();
 
-	ksu_throne_tracker_exit();
+	ksu_uid_observer_exit();
 
 	destroy_workqueue(ksu_workqueue);
-
-#ifdef CONFIG_KPROBES
-	ksu_ksud_exit();
-	ksu_sucompat_exit();
-#endif
 
 	ksu_core_exit();
 }
